4cbc1e8d756ed83556f5891dfaea14d891fcbc01
柯灰 <i@kohai.top>
2022-01-27 10:55:56 +0800

    Update juejin.js

--- src/juejin.js
+++ src/juejin.js
@@ -6,6 +6,17 @@ const cookie = process.env.JUEJIN_COOKIE;
 const axios = new Axios({
   headers: {
     cookie,
+    origin: 'https://juejin.cn',
+    pragma: 'no-cache',
+    referer: 'https://juejin.cn/',
+    'sec-ch-ua':
+        '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
   },
   withCredentials: true,
 });
